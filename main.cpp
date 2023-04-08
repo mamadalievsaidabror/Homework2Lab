0c0beae3a5b5f57dc87acf6f227a04b96e8c97fa
Student of NEWUU <student@newuu.local>
2023-04-08 15:43:48 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+/* Problem 1
+int a, b;
+cout << "Enter 1st Number:" << endl;
+cin >> a;
+cout << "Enter 2nd Number:" << endl;
+cin >> b;
+int c = a + b;
+int d = a * b;
+cout << "Sum:" << c << endl << "Multiplication: " << d << endl;
+*/
+
+/* Problem 2
+int r;
+double pi = 3.14;
+cout << "Enter Radius of Circle:" << endl;
+cin >> r;
+double d = 2 * r;
+double l = 2 * pi * r;
+double s = pi * r * r;
+cout << "Diameter:" << d << "\nCircumference: " << l << "\nArea:" << s << endl;
+    */
+
+/* Problem 3
+ double Celsius;
+cout << "Enter  Temperature in Celsius: ";
+ cin >> Celsius;
+ double Farenheit = (Celsius * 1.8) + 32;
+ cout << "The temperature in Farenheit: " << Farenheit;
+
+*/
+
+/* Problem 4
+ int N;
+ cout << "Enter Number of Students: " << endl;
+  cin >> N;
+int l = 24 - (N % 24);
+ cout << "In Lab : " << l << " free computers out of 24 computers.";
+
+*/
+
+/* Problem 5
+int a, b;
+ cout << "Enter 1st integer:" << endl;
+ cin >> a;
+   cout << "Enter 2nd integer:" << endl;
+   cin >> b;
+   if (a >= b) {
+       cout << "The answer is 1";
+ }
+  else {
+     cout << "The answer is 0";
+    }
+    return 0;
+    */
+}
